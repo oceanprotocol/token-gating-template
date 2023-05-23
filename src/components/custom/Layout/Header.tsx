2d238e9036e98b6744f013b4ae67c8ff9b4d674c
Neagu Toma <toma.neagu@zitec.com>
2023-05-23 13:25:46 +0300

    refactor: update logo on mobile, and localePath for i18n

--- src/components/custom/Layout/Header.tsx
+++ src/components/custom/Layout/Header.tsx
@@ -10,7 +10,7 @@ import styles from './styles.module.scss';
 import personLogo from '../../../assets/person-circle.svg';
 import walletLogo from '../../../assets/globeSettings.svg';
 import AcceptedChains from '../WalletAuth/AcceptedChain';
-import logoHeader from '../../../assets/logoMobile.svg';
+import logoHeader from "../../../assets/logo-mobile.svg";
 import ConnectWalletModal from '../ConnectWalletModal';
 import NavBar from '../../../assets/expandLogo.svg';
 import useHeader from './hooks/useHeader';
@@ -43,26 +43,49 @@ const Header: NextPage<HeaderPropsType> = ({ toggleMenu, isExpanded }) => {
     <>
       <ConnectWalletModal showModal={showModal} onClose={closeModal} />
       <div>
-        <nav className={cx(styles.bgHeader, 'navbar navbar-expand-lg navbar-black bg-black px-3')}>
+        <nav
+          className={cx(
+            styles.bgHeader,
+            "navbar navbar-expand-lg navbar-black bg-black px-3"
+          )}
+        >
           <div className="d-flex flex-row justify-content-between w-100 py-3">
             <div className="w-100 w-md-auto me-0 me-md-5">
-              <button type="button" className="clean-empty-button" onClick={toggleMenu}>
+              <button
+                type="button"
+                className="clean-empty-button"
+                onClick={toggleMenu}
+              >
                 <Image
-                  className={cx(isExpanded ? styles.expandButton : styles.nonExpandButton, 'd-none d-md-flex')}
+                  className={cx(
+                    isExpanded ? styles.expandButton : styles.nonExpandButton,
+                    "d-none d-md-flex"
+                  )}
                   src={NavBar}
                   alt="navbar logo"
                 />
               </button>
               <div className="d-flex flex-row d-md-none justify-content-between align-items-center">
-                <Image src={logoHeader} alt="navbar logo" className="ms-2" />
+                <Image
+                  src={logoHeader}
+                  alt="navbar logo"
+                  className={cx(styles.mobileHeaderLogo, "ms-2")}
+                />
                 <div
-                  className={cx(styles.burgerButton, 'd-flex d-md-none order-0 me-2')}
+                  className={cx(
+                    styles.burgerButton,
+                    "d-flex d-md-none order-0 me-2"
+                  )}
                   onClick={handleBurgerMenuClick}
                 />
               </div>
             </div>
             <AcceptedChains />
-            <div className={cx('collapse navbar-collapse d-md-flex justify-content-end d-none')}>
+            <div
+              className={cx(
+                "collapse navbar-collapse d-md-flex justify-content-end d-none"
+              )}
+            >
               <ul className="navbar-nav">
                 <WalletAuth openModal={openModal} />
                 <Link className="nav-item mx-3 d-none d-md-flex" href={profile}>
@@ -75,11 +98,17 @@ const Header: NextPage<HeaderPropsType> = ({ toggleMenu, isExpanded }) => {
             </div>
           </div>
           {isBurgerMenuOpen && (
-            <div className={cx(styles.mobileHeaderContainer, styles.bgHeader, 'd-flex d-md-none')}>
+            <div
+              className={cx(
+                styles.mobileHeaderContainer,
+                styles.bgHeader,
+                "d-flex d-md-none"
+              )}
+            >
               <div
                 className={cx(
                   styles.mobileHeaderLinks,
-                  'd-flex d-md-none flex-column justify-content-start align-items-center w-100 mb-5',
+                  "d-flex d-md-none flex-column justify-content-start align-items-center w-100 mb-5"
                 )}
               >
                 {NavigationLinks.map((link) => {
@@ -88,11 +117,20 @@ const Header: NextPage<HeaderPropsType> = ({ toggleMenu, isExpanded }) => {
                       <Link
                         href={link.href}
                         onClick={handleBurgerClose}
-                        className={cx(styles.linkMobile, 'd-flex flex-row my-2')}
+                        className={cx(
+                          styles.linkMobile,
+                          "d-flex flex-row my-2"
+                        )}
                       >
-                        {link.index !== undefined && <Lock serviceIndex={link.index} />}
-                        {link.image && <Image src={link.image} alt="lock image" />}
-                        <div className="ms-3 UrbanistRegular16">{t(`${link.title}`)}</div>
+                        {link.index !== undefined && (
+                          <Lock serviceIndex={link.index} />
+                        )}
+                        {link.image && (
+                          <Image src={link.image} alt="lock image" />
+                        )}
+                        <div className="ms-3 UrbanistRegular16">
+                          {t(`${link.title}`)}
+                        </div>
                       </Link>
                     </React.Fragment>
                   );
